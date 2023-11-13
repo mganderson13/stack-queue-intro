13ec0681e95fe2a75555ace137cddc5d9dacb238
mganderson13 <144147921+mganderson13@users.noreply.github.com>
2023-11-13 14:00:12 -0600

    Initialize solution

---
+++ Queue.js
@@ -0,0 +1,52 @@
+/**
+ * This is an array implementation of a Queue,
+ * which is a FIFO linear data structure.
+ * @template T - the type of element stored in the queue
+ */
+class Queue {
+  /**
+   * Creates a Queue with optional initial elements.
+   * @param {T[]} arr
+   */
+  constructor(arr = []) {
+    this.arr = arr;
+  }
+
+  /**
+   * Adds `element` to the back of the queue
+   * @param {T} element
+   */
+  enqueue(element) {
+    this.arr.push(element);
+  }
+
+  /**
+   * Removes and returns the element at the front of the queue,
+   * or `undefined` if the queue is empty.
+   * @returns {T|undefined}
+   */
+  dequeue() {
+    return this.arr.shift();
+  }
+
+  /**
+   * Returns the element at the front of the queue _without_ removing it,
+   * or undefined if the queue is empty.
+   * @returns {T|undefined}
+   */
+  peek() {
+    return this.arr.at(0);
+  }
+
+  /** @returns {boolean} whether the queue is empty. */
+  isEmpty() {
+    return this.arr.length === 0;
+  }
+
+  /** @returns {number} the number of elements in the queue */
+  size() {
+    return this.arr.length;
+  }
+}
+
+module.exports = Queue;
---
+++ Stack.js
@@ -0,0 +1,52 @@
+/**
+ * This is an array implementation of a Stack,
+ * which is a LIFO linear data structure.
+ * @template T - the type of element stored in the stack
+ */
+class Stack {
+  /**
+   * Creates a Stack with optional initial elements.
+   * @param {T[]} arr
+   */
+  constructor(arr = []) {
+    this.arr = arr;
+  }
+
+  /**
+   * Adds `element` to the top of the stack.
+   * @param {T} element
+   */
+  push(element) {
+    this.arr.push(element);
+  }
+
+  /**
+   * Removes and returns the element at the top of the stack,
+   * or `undefined` if the stack is empty.
+   * @returns {T|undefined}
+   */
+  pop() {
+    return this.arr.pop();
+  }
+
+  /**
+   * Returns the element at the top of the stack _without_ removing it,
+   * or undefined if the stack is empty.
+   * @returns {T|undefined}
+   */
+  peek() {
+    return this.arr.at(-1);
+  }
+
+  /** @returns {boolean} whether the stack is empty. */
+  isEmpty() {
+    return this.arr.length === 0;
+  }
+
+  /** @returns {number} the number of elements in the stack. */
+  size() {
+    return this.arr.length;
+  }
+}
+
+module.exports = Stack;
